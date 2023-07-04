5e3cb985c2a89d56bc9e89b21b3b3850116f9724
ianabreu <ianabreudev@gmail.com>
2023-07-04 06:25:58 -0300

    configurações iniciais do projeto + instalação do styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import * as C from "./App.styles";
+import { Item } from "./types/Item";
 
-function App() {
+const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, title: "Comprar pão", done: false },
+    { id: 2, title: "criar video", done: true },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+        {list.map((item, index) => (
+          <div>{item.title}</div>
+        ))}
+      </C.Area>
+    </C.Container>
   );
-}
-
+};
 export default App;
